e7719f7cbf906a96ac58537d8c68454f727ac793
Jon Skinner <jon.a.skinner1@gmail.com>
2019-03-29 00:07:55 -0400

    New logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
     <nav id="header" class="navbar navbar-expand-lg fixed-top navbar-light bg-white">
         <div class="container">
-            <span class="navbar-brand mb-0">Super Pretendo</span>
+            <a class="navbar-brand" href="#">
+                <img src="../assets/images/super-pretendo-logo.svg" alt="">
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -22,34 +24,24 @@
         components: {Channels}
     }
 
-    window.onscroll = function () {
-        scrollFunction()
-    };
+    // window.onscroll = function () {
+    //     scrollFunction()
+    // };
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            document.getElementById("header").classList.add('header-collapsed')
-        } else {
-            document.getElementById("header").classList.remove('header-collapsed')
-        }
-    }
+    // function scrollFunction() {
+    //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //         document.getElementById("header").classList.add('header-collapsed')
+    //     } else {
+    //         document.getElementById("header").classList.remove('header-collapsed')
+    //     }
+    // }
 </script>
 
 <style scoped lang="scss">
     #header {
-
-        .navbar-brand {
-            font-family: league-gothic, sans-serif;
-            font-style: italic;
-            font-weight: 700;
-            color: #43b3d8;
-            transition: 0.5s;
-        }
-
-        &.header-collapsed {
-            .navbar-brand {
-                font-size: 2rem;
-            }
+        .navbar-brand img {
+            height: 60px;
+            transition: all .5s;
         }
     }
 </style>
